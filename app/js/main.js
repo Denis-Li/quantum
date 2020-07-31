047346d1e0f64fcffb8cda2e7fd38f34efc19f63
Denis-Li <den.le.genplan@gmail.com>
2020-07-31 17:33:00 +0200

    add slick

---
+++ app/js/main.js
@@ -0,0 +1,7 @@
+$(function () {
+    $('.banner-section__slider').slick({
+        dots: true,
+        prevArrow: '<button class="banner-section__slider-btn banner-section__slider-btnprev"><img src="img/carousel/arrow_left.svg" alt=""></button>',
+        nextArrow: '<button class="banner-section__slider-btn banner-section__slider-btnnext"><img src="img/carousel/arrow_right.svg" alt=""></button>',
+    });
+  });
--- js/main.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function(){
-    $("#carousel-header").owlCarousel({
-        items: 1,
-        nav: true,
-        navText: [],
-        loop: true
-    });
-  });
